8b872fa67939cb10c55eebd22c10dc0b223c4725
Christoph Rauch <christoph.rauch@gmail.com>
2013-09-12 14:45:08 +0200

    Tests
     * need a mocking framework

--- setup.py
+++ setup.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-import fastbill
-version = str(fastbill.__version__)
+# fix for TypeError: 'NoneType' object is not callable
+import multiprocessing
+
+version = '0.1.2'
 
 setup(
     name='fastbill',
     version=version,
     description='A thin python wrapper for the fastbill API',
-    #TODO: write README.txt
-    #long_description=open('./README.txt', 'r').read(),
+    long_description=open('README.md', 'r').read(),
     author='Dimitar Roustchev',
     author_email='dimitar.roustchev@stylight.com',
     url='http://github.com/stylight/stylight-fastbill',
-    #TODO: Verify license
     license='MIT License',
-    packages=find_packages(),
+    packages=['fastbill'],
     install_requires=[
-        'requests==0.14.2'
+        'requests==0.14.2',
     ],
-    test_requires=[
-        'nose==1.3.0'
-    ],
+    setup_requires=["nose>=1.0", "httpretty==0.6.3"],
+    test_suite="nose.collector",
     keywords='fastbill api'
 )
