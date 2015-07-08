15e3b77e1458686e6c73544b145803a76dc6c2b2
Christoph Rauch <smokey42@users.noreply.github.com>
2015-07-08 10:37:10 +0200

    Fix tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -40,7 +40,7 @@ class JsonTest(unittest.TestCase):
             'date': datetime.date(2016, 6, 2),
             'datetime': datetime.datetime(2015, 5, 1, 14, 42, 17),
             'money': decimal.Decimal("17.23"),
-        }, cls=fastbill.CustomJsonEncoder)
+        }, cls=fastbill.jsonencoder.CustomJsonEncoder)
 
         self.assertEqual(
             json_dump,
@@ -84,9 +84,9 @@ class TestWrapper(unittest.TestCase):
 
         class FakeAPI(object):
             def subscription_get(self, filter=None):
-                return fastbill.FastbillResponse(response, self)
+                return fastbill.response.FastbillResponse(response, self)
 
-        resp = fastbill.FastbillResponse(response, FakeAPI())
+        resp = fastbill.response.FastbillResponse(response, FakeAPI())
         self.assertEquals(response,
                           resp.subscriptions[0].subscription.subscription)
         self.assertRaises(AttributeError, getattr, resp, 'blah')
@@ -132,7 +132,7 @@ class TestWrapper(unittest.TestCase):
                     return (http_code, headers, json.dumps({
                         'RESPONSE': response,
                         'REQUEST': request,
-                    }, cls=fastbill.CustomJsonEncoder))
+                    }, cls=fastbill.jsonencoder.CustomJsonEncoder))
 
                 httpretty.register_uri(httpretty.POST,
                                        fastbill.FastbillWrapper.SERVICE_URL,
@@ -143,7 +143,7 @@ class TestWrapper(unittest.TestCase):
                     result = method(**params)
                     self.assertEqual(result, response)
                 else:
-                    self.assertRaises(fastbill.FastbillResponseError,
+                    self.assertRaises(fastbill.exceptions.FastbillResponseError,
                                       method, **params)
 
 
@@ -172,7 +172,7 @@ class TestWrapper(unittest.TestCase):
         import fastbill
 
         api = fastbill.FastbillWrapper(api_email, api_key)
-        response = fastbill.FastbillResponse(RESPONSE_DATA, api)
+        response = fastbill.response.FastbillResponse(RESPONSE_DATA, api)
         pickled_response = pickle.dumps(response)
         unpickled_response = pickle.loads(pickled_response)
         self.assertTrue(unpickled_response.api is None)
